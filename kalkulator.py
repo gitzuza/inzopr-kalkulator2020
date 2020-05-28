4e8a94f673666ac5b828b59ac693ec78d4aa0d79
Zuzanna <you@example.com>
2020-05-29 00:40:25 +0200

    funkcja dodawania poprawiona i gotowa

--- kalkulator.py
+++ kalkulator.py
@@ -1,9 +1,8 @@
-def dodaj();
-print ("wprowadz dwie liczby a zostana one dodane")
-    a= int(input())
-    b=int(input())
-    print(a+b)
-    
- dodaj()
+def dodaj(a, b):
+    wynik = a + b 
+    return wynik
+   a=int(input()) 
+    b=int(input()) 
+ print(dodaj(a, b))
     
     
