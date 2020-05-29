29402679e351326dc029a2af6fbee1bebc25a2c5
Zuzanna <you@example.com>
2020-05-29 16:45:06 +0200

    Funcja get/info gotowa

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,13 @@
+def get_info():
+print("WItaj to jest kalkulator")
+
 def dodaj(a, b):
     wynik = a + b 
     return wynik
    a=int(input()) 
     b=int(input()) 
- print(dodaj(a, b))
-    
-    
+ print(a+b)
+ 
+ 
+ get_info()   
+    dodaj()
